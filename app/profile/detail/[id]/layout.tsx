e6a38e3ee1bc6018f06175fcc5dc7df3e416b910
saepulfariz <saepulfariz108@gmail.com>
2025-07-10 10:05:08 +0700

    add state in profile detail page and fix title layout

---
+++ app/profile/detail/[id]/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Profile Detail",
+};
+
+export default function DetailLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
--- app/profile/detail/[id]/page.tsx
+++ app/profile/detail/[id]/page.tsx
@@ -1,17 +1,29 @@
+"use client";
+
+// File: app/profile/detail/[id]/page.tsx
+// This file is used to display the details of a profile based on the ID passed in the URL.';
 import Link from "next/link";
 
-export const metadata = {
-  title: "Profile Detail",
-};
 
-export default function Page({ params }: { params: { id: string } }) {
+import { use, useState } from "react";
+
+export default function Page({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = use(params);
+  const [count, setCount] = useState(0);
   return (
     <main>
       <h1 className="text-2xl font-bold">Profile Detail Page</h1>
       <p className="mt-4">
-        This is the profile detail page member with ID: {params.id}
+        This is the profile detail page member with ID: {id}
       </p>
-
+      <p>Count : {count}</p>
+      <button
+        className="mt-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment
+      </button>
+      <br />
       <Link className="text-blue-500 hover:underline" href="/profile/detail">
         Back
       </Link>
